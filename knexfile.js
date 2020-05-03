4c633494c06a6030b88a196bd26101cdad9eaf81
fanchangyong <fanchangyong@gmail.com>
2020-05-03 19:35:57 +0800

    修改了连接字符串

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-  client: 'mysql',
-  connection: process.env.MYSQL_URL,
+  client: "mysql",
+  // connection: process.env.MYSQL_URL,
+  connection: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "12345678",
+    database: "xiangbai",
+  },
 };
