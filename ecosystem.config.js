554d27526b38cc8341a5464e3a7ea919e2952453
xuwenjiang <silence600@hotmail.com>
2020-05-06 15:16:06 +0800

    fix pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,25 +11,13 @@ module.exports = {
         "logs",
       ],
       instances: 2,
-      max_memory_restart: 1,
-      error_file: "./logs/error.log",
       autorestart: true,
       NODE_ENV: "production",
-      merge_logs: true,
     },
   ],
 
   deploy: {
     production: {
-      user: "SSH_USERNAME",
-      host: "SSH_HOSTMACHINE",
-      ref: "origin/master",
-      repo: "GIT_REPOSITORY",
-      path: "DESTINATION_PATH",
-      "pre-deploy-local": "",
-      "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
     },
   },
 };
